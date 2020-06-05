627825eec5059988c9645327d77c784029ec07b4
ZaviiNet <trancefx@live.co.uk>
2020-06-05 15:30:23 +0100

    Push

---
+++ Apex Password Tool/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+namespace Apex_Password_Generator
+{
+    public partial class MainWindow : Window
+    {
+
+        char[] chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!£$%^&*()@{}:~<>".ToArray();
+        int i = 10;
+        int previ = 10;
+        Random rand = new Random();        
+        public MainWindow()
+        {
+            InitializeComponent();
+            generate();
+            Telemetry.TrackEvent("Application Opened");
+        }
+        private void recalc(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                i = (int)Math.Round(numSlider.Value);
+                if(i != previ)
+                {
+                    generate();
+                    previ = i;
+                }
+                numText.Text = i.ToString() + " characters";
+            }catch(Exception ex) { }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            generate();
+            Telemetry.TrackEvent("Password Generated");
+        }
+        void generate()
+        {
+            string s = "";
+            for (int count=0;count<i;count++)
+            {
+                s += chars[rand.Next(0, chars.Length-1)];
+            }
+            passBox.Text = s;
+        }
+        private void mouseup(object sender, MouseButtonEventArgs e)
+        {
+            generate();
+        }
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            string s = "";
+            for (int count = 0; count < i; count++)
+            {
+                s += chars[rand.Next(0, chars.Length - 1)];
+            }
+            passBox.Text = s;
+            Clipboard.SetText(passBox.Text);
+            Telemetry.TrackEvent("Password Generated & Copied");
+        }
+        
+    }
+    
+}
---
+++ Apex Password Tool/Telemetry.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Diagnostics;
+using System.Reflection;
+
+
+    public static class Telemetry
+    {
+        private const string TelemetryKey = "233dba19-dd68-4a1f-abdd-1a5f923b48b7";
+
+        private static TelemetryClient _telemetry = GetAppInsightsClient();
+
+        public static bool Enabled { get; set; } = true;
+
+        private static TelemetryClient GetAppInsightsClient()
+        {
+            var config = new TelemetryConfiguration();
+            config.InstrumentationKey = TelemetryKey;
+            config.TelemetryChannel = new Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel.ServerTelemetryChannel();
+            //config.TelemetryChannel = new Microsoft.ApplicationInsights.Channel.InMemoryChannel(); // Default channel
+            config.TelemetryChannel.DeveloperMode = Debugger.IsAttached;
+            #if DEBUG
+            config.TelemetryChannel.DeveloperMode = true;
+        #endif
+            TelemetryClient client = new TelemetryClient(config);
+            client.Context.Component.Version = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            client.Context.Session.Id = Guid.NewGuid().ToString();
+            client.Context.User.Id = (Environment.UserName + Environment.MachineName).GetHashCode().ToString();
+            client.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+            return client;
+        }
+
+        public static void SetUser(string user)
+        {
+            _telemetry.Context.User.AuthenticatedUserId = user;
+        }
+
+        public static void TrackEvent(string key, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null)
+        {
+            if (Enabled)
+            {
+                _telemetry.TrackEvent(key, properties, metrics);
+            }
+        }
+
+        public static void TrackException(Exception ex)
+        {
+            if (ex != null && Enabled)
+            {
+                var telex = new Microsoft.ApplicationInsights.DataContracts.ExceptionTelemetry(ex);
+                _telemetry.TrackException(telex);
+                Flush();
+            }
+        }
+
+        internal static void Flush()
+        {
+            _telemetry.Flush();
+        }
+    }
+
